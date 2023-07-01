203e00cb0482c8c4f6bfa0c0054dce4aa980ceeb
What-Question-Mark <97435840+What-Question-Mark@users.noreply.github.com>
2023-07-01 17:15:47 +1000

    update

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const greetings = [
-    "Hello there!",                  // English
-    "Bonjour là-bas!",               // French
-    "¡Hola ahí!",                    // Spanish
-    "Hallo dort!",                   // German
-    "Ciao lì!",                      // Italian
-    "こんにちは そこに！",            // Japanese
-    "거기 안녕!",                     // Korean
-    "你好那里！",                     // Mandarin Chinese
-    "مرحبا هناك!",                  // Arabic
-    "Sawubona lapho!"                // Zulu
+    "Hello there! <br><span id='greeting-language'>English</span>",                  // English
+    "Bonjour là-bas! <br><span id='greeting-language'>French</span>",               // French
+    "¡Hola ahí! <br><span id='greeting-language'>Spanish</span>",                    // Spanish
+    "Hallo dort! <br><span id='greeting-language'>German</span>",                   // German
+    "Ciao lì! <br><span id='greeting-language'>Italian</span>",                      // Italian
+    "こんにちは そこに！ <br><span id='greeting-language'>Japanese</span>",            // Japanese
+    "거기 안녕! <br><span id='greeting-language'>Korean</span>",                     // Korean
+    "你好那里！ <br><span id='greeting-language'>Mandarin Chinese</span>",                     // Mandarin Chinese
+    "مرحبا هناك! <br><span id='greeting-language'>Arabic</span>",                  // Arabic
+    "Sawubona lapho! <br><span id='greeting-language'>Zulu</span>"                // Zulu
 ];
 
 new Typewriter(document.getElementById("greeting"), {
